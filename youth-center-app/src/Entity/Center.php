09e70b57ce64b6c649074e6987f59e1efb2cdf3b
Amira el Manaa <amiraelmanaa13@gmail.com>
2025-07-09 16:25:02 +0200

    fixed the db issue

---
+++ youth-center-app/src/Entity/Center.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CenterRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: CenterRepository::class)]
+class Center
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private string $name;
+
+    #[ORM\Column(length: 255)]
+    private string $country;
+
+    #[ORM\Column(length: 255)]
+    private string $city;
+
+    #[ORM\Column(length: 255)]
+    private string $address;
+
+    #[ORM\Column(length: 255)]
+    private string $category;
+
+    #[ORM\Column(type: 'text', nullable: true)]
+    private ?string $description = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $phone = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $email = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+    public function getName(): string
+    {
+        return $this->name;
+    }
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+        return $this;   }
+    public function getCountry(): string
+    {
+        return $this->country;
+}
+    
+}
