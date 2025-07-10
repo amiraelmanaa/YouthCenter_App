50080ec35becd440bfc1aa57a1e906285ad65cc5
Amira el Manaa <amiraelmanaa13@gmail.com>
2025-07-11 01:10:50 +0200

    loginandregistertemplates

--- youth-center-app/src/Entity/User.php
+++ youth-center-app/src/Entity/User.php
@@ -89,13 +89,14 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     {
         return $this->password;
     }
-
+    
     public function setPassword(string $password): static
     {
         $this->password = $password;
 
         return $this;
     }
+    
 
     /**
      * Ensure the session doesn't contain actual password hashes by CRC32C-hashing them, as supported since Symfony 7.3.
